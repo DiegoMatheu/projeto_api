18b9b5db759cb289984f9d78908e39891568f870
Diego Matheus <diego.matheus9233@gmail.com>
2021-07-02 00:59:06 -0300

    Adicionei os metodos restantes e fiz a autorização por token

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,13 +2,13 @@ class AuthenticationController < ApplicationController
     def login
         user = User.find_by(email: params[:email])
         if (!user)
-            render status: :unauthorized
+            render json:{error: 'Email not found'},status: :unauthorized
         else 
             if user.authenticate(params[:password])
                 secret_key = Rails.application.secrets.secret_key_base[0]
                 token = JWT.encode({
                     user_id: user.id,
-                    user_name: user.name
+                    
                 }, secret_key )
                 render json: {token: token}
             else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,43 @@
 class UsersController < ApplicationController
+before_action :token_authenticate
     def index
         users = User.all()
-        render json: users
+        render json: {users: users}
     end
     def create
-        user = User.create(
-            name: params[:name],
-            email: params[:email],
-            password: params[:password],
-            cpf: params[:cpf]
-        )
-        render json: user
+        user = User.create(user_params)
+        if (user.valid?)
+            render json: {message: 'Created User'}
+        else
+            render json: {error: 'Not created'}, status: :unprocessable_entity
+        end
     end
+
+    def show 
+        user = User.find_by(params[:id])
+        render json: {user: user}
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        render json: { message: 'Deleted User'}, status: :no_content
+
+    end
+
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+            render json: { message: 'Update user',  user:user}
+        else
+            render json: { error: 'User not update', user:user.errors}, status: :unprocessable_entity
+        end
+    end
+
+    private 
+
+    def user_params
+        params.permit(:name,:email,:cpf,:password)
+    end
+
 end
